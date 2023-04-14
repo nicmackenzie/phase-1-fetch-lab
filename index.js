02ad4e14410d824765328768ff844380feabcf59
nicmackenzie <nicmackenzie@live.com>
2023-04-14 16:38:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(data => {
+      renderBooks(data);
+    });
 }
 
 function renderBooks(books) {
@@ -12,6 +16,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
